df72b3bdac3f41d15b1d3f87ac650e8af461f779
arnold1 <arnold1@gmail.com>
2015-05-28 19:13:52 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// from crates.io
+extern crate mmap;
+extern crate libc;
+
+use std::thread;
+use std::env;
+mod fpga_awg;
+
+fn main() {
+    if let Some(arg1) = env::args().nth(1) {
+        let mut awg = fpga_awg::new();
+
+        while (true) {
+            awg.toggle(1);
+            thread::sleep_ms(arg1.parse::<u32>().unwrap());
+        }
+    }
+}
